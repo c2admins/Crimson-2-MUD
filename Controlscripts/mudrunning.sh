435954ae64c70ee349354477f331cfa56c39375f
c2admins <68524648+c2admins@users.noreply.github.com>
2020-07-19 22:50:59 -0400

    C2 Control Scripts Upload

---
+++ Controlscripts/mudrunning.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+case "$(pidof bin/crimsond | wc -w)" in
+
+0) 
+echo "Restarting MUD all ports $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+# Restarts Production Port
+rm /home/ubuntu/c2/controlscripts/.prodlockfile
+cd /home/ubuntu/c2/controlscripts
+./start_prod
+# Restarts Zone Port
+rm /home/ubuntu/c2/controlscripts/.devlockfile
+cd /home/ubuntu/c2/controlscripts
+./start_dev
+;;
+1) # One servers is not up
+echo "Restarting down MUD port $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+if ! pgrep -x .autorunprod.sh > /dev/null
+then
+    echo "  Restarting Production port $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+rm /home/ubuntu/c2/controlscripts/.prodlockfile
+cd /home/ubuntu/c2/controlscripts
+./start_prod
+fi
+if ! pgrep -x .autorundev.sh > /dev/null
+then
+    echo "  Restarting Zone port $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+rm /home/ubuntu/c2/controlscripts/.devlockfile
+cd /home/ubuntu/c2/controlscripts
+./start_dev
+fi
+;;
+2) # All is OK
+   echo " All ports are up $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+;;
+3) # Test or Hellnight Port is up
+   echo " All ports are up $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+;;
+*) echo "Something is wrong: $(date)">>/home/ubuntu/c2/controlscripts/mudrunning.txt
+;;
+esac
