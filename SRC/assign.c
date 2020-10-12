0a248d7f122be69b288dd96951788f332b7118f3
c2admins <68524648+c2admins@users.noreply.github.com>
2020-10-11 20:21:23 -0400

    Brimstone City Pet Shop

--- SRC/assign.c
+++ SRC/assign.c
@@ -325,7 +325,7 @@ void as1200_assign_rooms(void)
 	ASSIGN_ROOM(4513, cliff);
 
 	ASSIGN_ROOM(3031, pr3600_pet_shops);
-	ASSIGN_ROOM(21833, pr3750_brim_pet_shops); //Brimstone City Pet Shop
+	ASSIGN_ROOM(21833, pr3750_brim_pet_shop); //Brimstone City Pet Shop
 	
 	ASSIGN_ROOM(17933, pc1500_glass_door);
 
